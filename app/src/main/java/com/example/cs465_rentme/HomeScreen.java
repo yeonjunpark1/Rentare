091eaf85ebf72b1b6625e23f55b9f7f1bad8330c
Isaac Adorno <isaacadorno21@gmail.com>
2019-11-13 15:49:03 -0600

    Worked on SignUp layout, created HomeScreen Activity and XML Layout

---
+++ app/src/main/java/com/example/cs465_rentme/HomeScreen.java
@@ -0,0 +1,14 @@
+package com.example.cs465_rentme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_screen);
+    }
+}
