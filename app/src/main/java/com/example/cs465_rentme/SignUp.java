29cec6ee45e9097fcbabb479c95a3e162476296e
Isaac Adorno <isaacadorno21@gmail.com>
2019-11-13 14:56:39 -0600

    Initial commit

---
+++ app/src/main/java/com/example/cs465_rentme/SignUp.java
@@ -0,0 +1,14 @@
+package com.example.cs465_rentme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
---
+++ app/src/main/java/com/example/cs465_rentme/Welcome.java
@@ -0,0 +1,14 @@
+package com.example.cs465_rentme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+    }
+}
