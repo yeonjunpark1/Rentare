5ea9f79b09950ac2af92d478707bc8abc77654a6
Isaac Adorno <isaacadorno21@gmail.com>
2019-11-14 12:03:55 -0600

    Map working! Current location is in Australia tho lol

--- app/src/main/java/com/example/cs465_rentme/HomeScreen.java
+++ app/src/main/java/com/example/cs465_rentme/HomeScreen.java
@@ -1,13 +1,25 @@
 package com.example.cs465_rentme;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class HomeScreen extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeScreen extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
+
+        Button mapButton = findViewById(R.id.map_button);
+        mapButton.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        Intent intent = new Intent(HomeScreen.this, Map.class);
+        startActivity(intent);
     }
 }
