00f41ef46e69868b23b13025f8734e05bbcc4f90
Isaac Adorno <isaacadorno21@gmail.com>
2019-11-15 10:47:23 -0600

    Map location changed to Siebel, markers and context window added

--- app/src/main/java/com/example/cs465_rentme/Map.java
+++ app/src/main/java/com/example/cs465_rentme/Map.java
@@ -3,15 +3,18 @@ package com.example.cs465_rentme;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Map extends FragmentActivity implements OnMapReadyCallback {
+public class Map extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
 
     private GoogleMap mMap;
 
@@ -38,10 +41,19 @@ public class Map extends FragmentActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setMinZoomPreference(15);
+        mMap.setMaxZoomPreference(20);
+        mMap.setOnInfoWindowClickListener(this);
+        // Add a marker in Siebel and move the camera
+        mMap.addMarker(new MarkerOptions().position(new LatLng(40.116566, -88.222277)).
+                        title("BBQ Grill for Rent").snippet("$20 / day").icon(BitmapDescriptorFactory.fromResource(R.drawable.pin)));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(40.113858, -88.224896)).
+                        title("Your Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(40.113858, -88.224896)));
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        Toast.makeText(this, "Hello world!", Toast.LENGTH_SHORT).show();
     }
 }
