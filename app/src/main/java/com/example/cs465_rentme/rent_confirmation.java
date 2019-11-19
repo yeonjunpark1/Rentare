06090c7de4ea5b3180f3a2ea92e56922818059bb
YJ <yjpark409@icloud.com>
2019-11-18 18:58:09 -0600

    Rent Confirmation Page

---
+++ app/src/main/java/com/example/cs465_rentme/rent_confirmation.java
@@ -0,0 +1,14 @@
+package com.example.cs465_rentme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class rent_confirmation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rent_confirmation);
+    }
+}
