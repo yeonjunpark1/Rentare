b79d4fc2c357b6bbb70274e4b09a14c425607ab8
YJ <yjpark409@icloud.com>
2019-11-18 18:13:17 -0600

    Rent Page 2 Complete

---
+++ app/src/main/java/com/example/cs465_rentme/rent_page_two.java
@@ -0,0 +1,14 @@
+package com.example.cs465_rentme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class rent_page_two extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rent_page_two);
+    }
+}
