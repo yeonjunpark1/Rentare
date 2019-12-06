ae5b66f04361ae807e240ad3a120fffb8ac105ff
YJ <yjpark409@icloud.com>
2019-12-06 16:57:18 -0600

    Move map button Rent List Page

--- app/src/main/java/com/example/cs465_rentme/HomeScreen.java
+++ app/src/main/java/com/example/cs465_rentme/HomeScreen.java
@@ -14,11 +14,9 @@ public class HomeScreen extends AppCompatActivity implements View.OnClickListene
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
 
-        Button mapButton = findViewById(R.id.map_button);
         Button lendButton = findViewById(R.id.lend_button);
         Button rentButton = findViewById(R.id.rent_button);
         Button settingsButton = findViewById(R.id.settings_button);
-        mapButton.setOnClickListener(this);
         lendButton.setOnClickListener(this);
         rentButton.setOnClickListener(this);
         settingsButton.setOnClickListener(this);
@@ -26,8 +24,7 @@ public class HomeScreen extends AppCompatActivity implements View.OnClickListene
 
     public void onClick(View v) {
         Intent intent = null;
-        if (v.getId() == R.id.map_button) intent = new Intent(HomeScreen.this, Map.class);
-        else if (v.getId() == R.id.lend_button) intent = new Intent(HomeScreen.this, lend_list.class);
+        if (v.getId() == R.id.lend_button) intent = new Intent(HomeScreen.this, lend_list.class);
         else if (v.getId() == R.id.rent_button) intent = new Intent(HomeScreen.this, rent_list.class);
         else if (v.getId() == R.id.settings_button) intent = new Intent(HomeScreen.this, user_page.class);
         startActivity(intent);
--- app/src/main/java/com/example/cs465_rentme/rent_list.java
+++ app/src/main/java/com/example/cs465_rentme/rent_list.java
@@ -7,12 +7,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 
-public class rent_list extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+public class rent_list extends AppCompatActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener{
 
 
 
@@ -21,12 +22,15 @@ public class rent_list extends AppCompatActivity implements AdapterView.OnItemSe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rent_list);
 
+        Button map2Button = findViewById(R.id.map2_button);
+        map2Button.setOnClickListener(this);
 
         Spinner spinner = findViewById(R.id.category_spinner);
         Spinner sort_spin = findViewById(R.id.sort_spinner);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.category_choices,android.R.layout.simple_spinner_item);
         ArrayAdapter<CharSequence> adapter_sort = ArrayAdapter.createFromResource(this, R.array.sort_choices,android.R.layout.simple_spinner_item);
+
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         adapter_sort.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -37,6 +41,15 @@ public class rent_list extends AppCompatActivity implements AdapterView.OnItemSe
         spinner.setOnItemSelectedListener(this);
         sort_spin.setOnItemSelectedListener(this);
 
+
+        ImageView map_photo = findViewById(R.id.map_icon);
+        map_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(rent_list.this, Map.class);
+                startActivity(intent);
+            }
+        });
 
         ImageView grill_photo = findViewById(R.id.grill_image_click);
         grill_photo.setOnClickListener(new View.OnClickListener() {
@@ -54,11 +67,17 @@ public class rent_list extends AppCompatActivity implements AdapterView.OnItemSe
         String text = parent.getItemAtPosition(position).toString();
         Toast.makeText(parent.getContext(),text, Toast.LENGTH_SHORT).show();
 
-
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    public void onClick(View v) {
+        Intent intent = null;
+        if (v.getId() == R.id.map2_button) intent = new Intent(rent_list.this, Map.class);
+
+        startActivity(intent);
     }
 }
