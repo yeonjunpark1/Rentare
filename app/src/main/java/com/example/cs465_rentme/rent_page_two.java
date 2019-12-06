b1fad2ca00f693a5562436028aa71fafbe3f68b2
YJ <yjpark409@icloud.com>
2019-12-06 17:29:39 -0600

    Move map button Rent List Page

--- app/src/main/java/com/example/cs465_rentme/rent_page_two.java
+++ app/src/main/java/com/example/cs465_rentme/rent_page_two.java
@@ -12,9 +12,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class rent_page_two extends AppCompatActivity implements View.OnClickListener {
-
+    String first_date;
+    String second_date;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,8 +38,9 @@ public class rent_page_two extends AppCompatActivity implements View.OnClickList
     @RequiresApi(api = Build.VERSION_CODES.N)
     public void onClick(View v) {
         if(v.getId() == R.id.reserve_button) {
-
             Intent intent = new Intent(rent_page_two.this, rent_confirmation.class);
+            TextView tv1 = findViewById(R.id.total_price);
+            intent.putExtra("total_price", tv1.getText().toString());
             startActivity(intent);
         } else if(v.getId() == R.id.rent_start_date_button){
             final Calendar c = Calendar.getInstance();
@@ -41,19 +48,42 @@ public class rent_page_two extends AppCompatActivity implements View.OnClickList
             int month = c.get(Calendar.MONTH);
             int day = c.get(Calendar.DAY_OF_MONTH);
 
+
             DatePickerDialog datePickerDialog = new DatePickerDialog(rent_page_two.this,
                     new DatePickerDialog.OnDateSetListener() {
 
                         @Override
                         public void onDateSet(DatePicker view, int year,
                                               int monthOfYear, int dayOfMonth) {
-                            TextView rent_start_date = findViewById(R.id.rent_start_date_button);
-                            rent_start_date.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
-
+                            TextView rent_start_date = findViewById(R.id.rent_start_date);
+                            if(dayOfMonth > 9) {
+                                rent_start_date.setText((monthOfYear + 1) + "-" + dayOfMonth + "-" + year);
+                            } else {
+                                rent_start_date.setText((monthOfYear + 1) + "-" + "0" + dayOfMonth + "-" + year);
+                            }
+                            TextView tv2 = findViewById(R.id.rent_start_date);
+                            TextView tv3 = findViewById(R.id.rent_end_date);
+                            first_date = tv2.getText().toString();
+                            second_date = tv3.getText().toString();
+                            if(!first_date.equals("Start Date") && !second_date.equals("End Date")) {
+                                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-uuuu");
+                                    LocalDate date1 = LocalDate.parse(first_date, formatter);
+                                    LocalDate date2 = LocalDate.parse(second_date, formatter);
+                                    long daysBetween = ChronoUnit.DAYS.between(date1, date2);
+                                    if(daysBetween > 0) {
+                                        long price = daysBetween * 20;
+                                        TextView tv1 = findViewById(R.id.total_price);
+                                        tv1.setText("Total Price: $" + price);
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), "Please Select Appropriate Dates", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            }
                         }
                     }, year, month, day);
-
             datePickerDialog.show();
+
         } else if(v.getId() == R.id.rent_end_date_button) {
             final Calendar c = Calendar.getInstance();
             int year = c.get(Calendar.YEAR);
@@ -65,14 +95,35 @@ public class rent_page_two extends AppCompatActivity implements View.OnClickList
                         @Override
                         public void onDateSet(DatePicker view, int year,
                                               int monthOfYear, int dayOfMonth) {
-                            TextView rent_end_date = findViewById(R.id.rent_end_date_button);
-                            rent_end_date.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                            TextView rent_end_date = findViewById(R.id.rent_end_date);
+                            if(dayOfMonth > 9) {
+                                rent_end_date.setText((monthOfYear + 1) + "-" + dayOfMonth + "-" + year);
+                            } else {
+                                rent_end_date.setText((monthOfYear + 1) + "-" + "0" + dayOfMonth + "-" + year);
+                            }
+                            TextView tv2 = findViewById(R.id.rent_start_date);
+                            TextView tv3 = findViewById(R.id.rent_end_date);
+                            first_date = tv2.getText().toString();
+                            second_date = tv3.getText().toString();
+                            if(!first_date.equals("Start Date") && !second_date.equals("End Date")) {
+                                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-uuuu");
+                                    LocalDate date1 = LocalDate.parse(first_date, formatter);
+                                    LocalDate date2 = LocalDate.parse(second_date, formatter);
+                                    long daysBetween = ChronoUnit.DAYS.between(date1, date2);
+                                    if(daysBetween > 0) {
+                                        long price = daysBetween * 20;
+                                        TextView tv1 = findViewById(R.id.total_price);
+                                        tv1.setText("Total Price: $" + price);
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), "Please Select Appropriate Dates", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            }
                         }
                     }, year, month, day);
-
             datePickerDialog.show();
+
         }
     }
-
-
 }
